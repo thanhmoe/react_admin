9d066c4e98bacd4e0a2f1d9e6c49425b066a6257
ThanhNC <thanhmoe24@gmail.com>
2024-08-09 14:52:23 +0700

    (login-traffic) add: Customer login trafic dashboard

--- src/services/customer_services.js
+++ src/services/customer_services.js
@@ -1,8 +1,11 @@
 import { axios_instance } from "../services/axios_config";
 
+const API_PATH = `/customers`
+
+
 export const fetchCustomers = async () => {
    try {
-      const response = await axios_instance.get("/customers");
+      const response = await axios_instance.get(API_PATH);
       return response.data;
    } catch (error) {
       return error.response.data;
@@ -11,9 +14,20 @@ export const fetchCustomers = async () => {
 
 export const updateCustomer = async () => {
    try {
-      const response = await axios_instance.get("/customers");
+      const response = await axios_instance.get(API_PATH);
       return response.data;
    } catch (error) {
       return error.response.data;
    }
 };
+
+export const getCustomerLoginTraffic = async(params) =>{
+   const {page, limit,startDate,endDate, interval,sortBy, sortOrder} = params
+	try {
+      const url = `${API_PATH}/loginTraffic?page=${page}&limit=${limit}&startDate=${startDate}&endDate=${endDate}&interval=${interval}&sortBy=${sortBy}&sortOrder=${sortOrder}`;
+		const response = await axios_instance.get(url);
+      return response.data
+	} catch (error) {
+		return error.response.data;
+	}
+}
